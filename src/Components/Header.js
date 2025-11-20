57522a37e57ab3b67b368a098d83f745094cf47c
ALI ALMOUKADDAM <alialmoukaddam@gmail.com>
2025-11-20 16:55:20 -0600

    v1.0.0

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles.css';
+
+export default function Header() {
+
+    return (
+        <div className="header">
+            <img className='logo' src="/logo.png" alt="Moviedux"/>
+            <h2 className='app-subtitle'>It's time to popcorn! Find your next movie here.</h2>
+        </div>
+    );
+}
